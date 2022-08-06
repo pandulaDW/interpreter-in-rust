7f620b3042ebe4a434fecaed86033817b689029d
pandulaDW <pandula.official@gmail.com>
2022-08-06 16:06:14 +1000

    Added support for identifiers and keywords

--- src/lexer.rs
+++ src/lexer.rs
@@ -29,21 +29,39 @@ impl Lexer {
     /// Returns the current token and reads the next token which will be returned on the
     /// next call
     pub fn next_token(&mut self) -> Token {
-        // read the first character of the input
+        // reading the first character of the input
         if self.read_position == 0 {
             self.read_char();
         }
 
+        // skipping whitespace characters
+        while self.ch.is_ascii_whitespace() {
+            self.read_char();
+        }
+
+        // get the matching token
         let tok = match self.ch {
-            '=' => new_token(TokenType::Assign, self.ch),
-            ';' => new_token(TokenType::Semicolon, self.ch),
-            '(' => new_token(TokenType::Lparen, self.ch),
-            ')' => new_token(TokenType::Rparen, self.ch),
-            ',' => new_token(TokenType::Comma, self.ch),
-            '+' => new_token(TokenType::Plus, self.ch),
-            '{' => new_token(TokenType::Lbrace, self.ch),
-            '}' => new_token(TokenType::Rbrace, self.ch),
-            _ => new_token(TokenType::Eof, NULL_CHAR),
+            '=' => new_token_from_ch(TokenType::Assign, self.ch),
+            ';' => new_token_from_ch(TokenType::Semicolon, self.ch),
+            '(' => new_token_from_ch(TokenType::Lparen, self.ch),
+            ')' => new_token_from_ch(TokenType::Rparen, self.ch),
+            ',' => new_token_from_ch(TokenType::Comma, self.ch),
+            '+' => new_token_from_ch(TokenType::Plus, self.ch),
+            '{' => new_token_from_ch(TokenType::Lbrace, self.ch),
+            '}' => new_token_from_ch(TokenType::Rbrace, self.ch),
+            NULL_CHAR => new_token_from_ch(TokenType::Eof, NULL_CHAR),
+            _ => {
+                if is_letter(self.ch) {
+                    let identifier = self.read_identifier();
+                    let token_type = look_up_identifier(&identifier);
+                    new_token_from_string(token_type, identifier)
+                } else if self.ch.is_digit(10) {
+                    let num_literal = self.read_number();
+                    new_token_from_string(TokenType::Int, num_literal)
+                } else {
+                    new_token_from_ch(TokenType::Illegal, self.ch)
+                }
+            }
         };
 
         // read the next character
@@ -53,7 +71,7 @@ impl Lexer {
     }
 
     /// Sets the next character and advances the position in the input
-    pub fn read_char(&mut self) {
+    fn read_char(&mut self) {
         if self.read_position >= self.input.len() {
             self.ch = NULL_CHAR;
         } else {
@@ -62,4 +80,36 @@ impl Lexer {
         self.position = self.read_position;
         self.read_position += 1;
     }
+
+    /// Reads an identifier by iteratively calling the read_char method and returns the string literal
+    fn read_identifier(&mut self) -> String {
+        let current_position = self.position;
+        while is_letter(self.peek_char()) {
+            self.read_char();
+        }
+        self.input[current_position..self.read_position]
+            .iter()
+            .collect()
+    }
+
+    /// Reads a number by iteratively calling the read_char method and returns the number literal
+    fn read_number(&mut self) -> String {
+        let current_position = self.position;
+        while self.peek_char().is_digit(10) {
+            self.read_char();
+        }
+        self.input[current_position..self.read_position]
+            .iter()
+            .collect()
+    }
+
+    /// Returns the character corresponding to the read_position
+    fn peek_char(&self) -> char {
+        self.input[self.read_position]
+    }
+}
+
+/// Checks if the given character is an ASCII alphabetic character or an _ character
+fn is_letter(ch: char) -> bool {
+    ch.is_ascii_alphabetic() || ch == '_'
 }
--- src/token.rs
+++ src/token.rs
@@ -36,63 +36,52 @@ pub enum TokenType {
     Let,
 }
 
-/// Generates a new token based on the provided token type and the character
-pub fn new_token(token_type: TokenType, ch: char) -> Token {
+/// Returns a new token based on the provided token type and the character
+pub fn new_token_from_ch(token_type: TokenType, ch: char) -> Token {
     Token {
         token_type,
         literal: ch.to_string(),
     }
 }
 
+/// Returns a new token based on the provided token type and the literal
+pub fn new_token_from_string(token_type: TokenType, literal: String) -> Token {
+    Token {
+        token_type,
+        literal,
+    }
+}
+
+/// Checks the given identifier and returns the correct TokeType.
+pub fn look_up_identifier(ident: &str) -> TokenType {
+    match ident {
+        "fn" => TokenType::Function,
+        "let" => TokenType::Let,
+        _ => TokenType::Ident,
+    }
+}
+
 #[cfg(test)]
 mod tests {
-    use super::{Token, TokenType::*, NULL_CHAR};
+    use super::{new_token_from_ch, new_token_from_string, Token, TokenType::*, NULL_CHAR};
     use crate::lexer;
 
     #[test]
-    fn test_next_token() {
+    fn test_next_token_for_characters() {
         let input = "=+(){},;";
+        let mut l = lexer::Lexer::new(input);
 
         let test_cases: Vec<Token> = vec![
-            Token {
-                token_type: Assign,
-                literal: "=".to_string(),
-            },
-            Token {
-                token_type: Plus,
-                literal: "+".to_string(),
-            },
-            Token {
-                token_type: Lparen,
-                literal: "(".to_string(),
-            },
-            Token {
-                token_type: Rparen,
-                literal: ")".to_string(),
-            },
-            Token {
-                token_type: Lbrace,
-                literal: "{".to_string(),
-            },
-            Token {
-                token_type: Rbrace,
-                literal: "}".to_string(),
-            },
-            Token {
-                token_type: Comma,
-                literal: ",".to_string(),
-            },
-            Token {
-                token_type: Semicolon,
-                literal: ";".to_string(),
-            },
-            Token {
-                token_type: Eof,
-                literal: NULL_CHAR.to_string(),
-            },
+            new_token_from_ch(Assign, '='),
+            new_token_from_ch(Plus, '+'),
+            new_token_from_ch(Lparen, '('),
+            new_token_from_ch(Rparen, ')'),
+            new_token_from_ch(Lbrace, '{'),
+            new_token_from_ch(Rbrace, '}'),
+            new_token_from_ch(Comma, ','),
+            new_token_from_ch(Semicolon, ';'),
+            new_token_from_ch(Eof, NULL_CHAR),
         ];
-
-        let mut l = lexer::Lexer::new(input);
 
         for (i, tt) in test_cases.iter().enumerate() {
             let tok = l.next_token();
@@ -107,6 +96,73 @@ mod tests {
                 tt.literal, tok.literal,
                 "tests[{}] - token type wrong. expected={}, got={}",
                 i, tt.literal, tok.literal
+            );
+        }
+    }
+
+    #[test]
+    fn test_next_token_source_code() {
+        let input = "let five = 5;
+                    let ten = 10;
+                    let add = fn(x, y) {
+                        x + y;
+                    };
+                    let result = add(five, ten);";
+        let mut l = lexer::Lexer::new(input);
+
+        let test_cases = vec![
+            new_token_from_string(Let, "let".to_string()),
+            new_token_from_string(Ident, "five".to_string()),
+            new_token_from_ch(Assign, '='),
+            new_token_from_string(Int, "5".to_string()),
+            new_token_from_ch(Semicolon, ';'),
+            new_token_from_string(Let, "let".to_string()),
+            new_token_from_string(Ident, "ten".to_string()),
+            new_token_from_ch(Assign, '='),
+            new_token_from_string(Int, "10".to_string()),
+            new_token_from_ch(Semicolon, ';'),
+            new_token_from_string(Let, "let".to_string()),
+            new_token_from_string(Ident, "add".to_string()),
+            new_token_from_ch(Assign, '='),
+            new_token_from_string(Function, "fn".to_string()),
+            new_token_from_ch(Lparen, '('),
+            new_token_from_string(Ident, "x".to_string()),
+            new_token_from_ch(Comma, ','),
+            new_token_from_string(Ident, "y".to_string()),
+            new_token_from_ch(Rparen, ')'),
+            new_token_from_ch(Lbrace, '{'),
+            new_token_from_string(Ident, "x".to_string()),
+            new_token_from_ch(Plus, '+'),
+            new_token_from_string(Ident, "y".to_string()),
+            new_token_from_ch(Semicolon, ';'),
+            new_token_from_ch(Rbrace, '}'),
+            new_token_from_ch(Semicolon, ';'),
+            new_token_from_string(Let, "let".to_string()),
+            new_token_from_string(Ident, "result".to_string()),
+            new_token_from_ch(Assign, '='),
+            new_token_from_string(Ident, "add".to_string()),
+            new_token_from_ch(Lparen, '('),
+            new_token_from_string(Ident, "five".to_string()),
+            new_token_from_ch(Comma, ','),
+            new_token_from_string(Ident, "ten".to_string()),
+            new_token_from_ch(Rparen, ')'),
+            new_token_from_ch(Semicolon, ';'),
+            new_token_from_ch(Eof, NULL_CHAR),
+        ];
+
+        for (i, tt) in test_cases.iter().enumerate() {
+            let tok = l.next_token();
+
+            assert_eq!(
+                tt.literal, tok.literal,
+                "tests[{}] - token type wrong. expected={}, got={}",
+                i, tt.literal, tok.literal
+            );
+
+            assert_eq!(
+                tt.token_type, tok.token_type,
+                "tests[{}] - token type wrong. expected={:?}, got={:?}",
+                i, tt.token_type, tok.token_type
             );
         }
     }
